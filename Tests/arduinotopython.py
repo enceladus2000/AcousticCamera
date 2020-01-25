c4464b16481e17ed1f7ea603509a938d282bb069
aadhar218 <aadharsharma218@gmail.com>
2020-01-26 01:17:12 +0530

    updated python code

--- Tests/arduinotopython.py
+++ Tests/arduinotopython.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 import serial
 import time
 from time import sleep
-ser=serial.Serial('/dev/ttyACM0', baudrate=115200)
+try:
+	ser=serial.Serial('/dev/ttyACM0', baudrate=115200,timeout=1)
+except:
+	print("port not found")
+time.sleep(2)
 ser.flushInput()
 s1=[]                                            # empty list to store the data of sensor 1
 s2=[]                                               # empty list to store the data of sensor 2
-b=""
 a=[]
 s=""
-data =[]   
-n=int(input("Enter the no.of values to take at a time")  
-n=100                
+data =[]    
+n=500              
 for i in range(n):
 	b = ser.readline()         # read a byte string
 	string_n = b[0:len(b)-2].decode("utf-8")  # decode byte string into Unicode  
@@ -27,8 +29,12 @@ ser.close()
 print(s1)
 print(s2)
 c= [x for x in range(0,len(s1))]
-plt.plot(c,s1)                               	#plotting values of sensor against natural no.
-plt.plot(c,s2)
+fig,ax=plt.subplots()
+line=ax.plo(y_var)
+plt.subplot(c,s1,'ro')                               #plotting values of sensor against natural no.
+plt.subplot(c,s2)
+plt.ylabel("sensor values")
+plt.suptitle("values of both the sensors")
 plt.show()
       # add to the end of data list
          # wait (sleep) 0.1 seconds
