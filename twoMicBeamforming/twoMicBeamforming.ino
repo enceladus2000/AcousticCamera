77d678775516fa8aedf50a93c11b3b992538d4ad
DangerousTim <tpb2k99@gmail.com>
2020-01-27 01:06:56 +0530

    untested code for 2 mic and linear scan area

--- twoMicBeamforming/twoMicBeamforming.ino
+++ twoMicBeamforming/twoMicBeamforming.ino
@@ -4,27 +4,30 @@
    which is then sent on serial monitor to be plotted as a heatmap
 */
 //mic1 is on the same side as scan area element 1
-const int micPin1 = A0;
-const int micPin2 = A1;
+const int micPins[] = {A0, A1};
+const int numMics = sizeof(micPins) / sizeof(int);    //at least in this sketch, numMics is limited to 2
 
-const int numPoints = 40;       //number of points in scan area
+const int numPoints = 21;       //number of points in scan area, should be an odd number
 //all distances in meters
 const float pointStep = 0.10;       //distance between consecutive scan points
-const float scanDist = 2.00;       //distance of scanning area from mics
-const float micDist = 0.28;          //distance between two mics
+const float scanDist = 1.00;       //distance of scanning area from mics
+const float micDist = 0.29;          //distance between two mics
 //angle subtended by scan area = 2 * atan((numPoints/2)*pointStep / scanDist)
-float scanArea[numPoints];
+float scanArea[numPoints];          //stores results of beamforming
 
-const int fs = 20000;      //sampling freq
+const int fs = 10000;      //sampling freq
 const int c = 343;        //speed of sound (m/s)
 
-int Delays[2][numPoints];
+//Delays should be (and are from testing) symmetrical 
+//floats are necessary so as not to lose information during normalising.
+//floats might also be useful for upsampling tests
+float Delays[2][numPoints];   //stores delay for each mic and point in scan area
 int intensities[numPoints]; //array that stores final data
 
 IntervalTimer samplingTimer;
-const int numSamples = 500;   //total number of samples in waveform
-int sampleCounter;
-int samples[2][numSamples]; //stores 2 waveforms
+const int numSamples = 600;   //total number of samples in waveform
+volatile int sampleCounter;   //must be volatile because is used in both main() and ISR
+float samples[numMics][numSamples]; //stores 2 waveforms
 //to properly resolve one cycle of a waveform of frequency f, 
 // numsamples >= fs / f or f >= fs / numSamples should be true
 
@@ -35,47 +38,61 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
   calcDelays();
-  Serial.println("unnormalised delays: ");
-  printDelays();
-  normaliseDelays();
   Serial.println("normalised delays");
   printDelays();
-
+    
   analogReadResolution(ANALOG_READ_RESOLUTION);
   analogReadAveraging(ANALOG_READ_AVERAGING);
 }
 
 void loop() {
-//  Serial.println("Beginning sampling...");
-//  samplingBegin();
-//  while (sampleCounter < numSamples);   //waiting for sampling to finish
-
+  Serial.println("Beginning sampling...");
+  samplingBegin();
+  while (sampleCounter < numSamples);   //waiting for sampling to finish
+  Serial.println("Done sampling!");
+  
   //now process the signals
+  //remove the dc component
   for (int m = 0; m < 2; m++)
     centerWaveform(samples[m], numSamples);
 
   //update scanArea array with the results of beamforming
   Beamforming();
   for (int p = 0; p < numPoints; p++){
-    //Serialprint
+    Serial.println(scanArea[p], 10);
   }
+  
+  Serial.println("Pausing for 1s...");
+  delay(1000);
 }
 
 //please check this code for logical errors
 void Beamforming() {
   for (int p = 0; p < numPoints; p++){
-    int bfsigsize = numSamples - max(Delays[0][p], Delays[1][p]);
-    long bfSignal[bfsigsize];  //beamformed signal
-
-    for (int i = 0; i < bfsigsize; i++){
-      bfSignal[i] = samples[0][i+Delays[0][p]] + samples[1][i+Delays[1][p]];
+    int intdly[2];    //rounded int delays for array manipulation
+    float bfweights[2]; //weights for each mic when beamforming
+    
+    //calculate beamforming weights from waveform power
+    for (int m = 0; m < 2; m++){
+      bfweights[m] = sqrt(calcPower(samples[0], numSamples));
+      intdly[m] = round(Delays[m][p]);
     }
-    scanArea[p] = calcPower(bfSignal, bfsigsize);
+    int bfsigsize = numSamples - max(intdly[0], intdly[1]);   //size of beamformed waveform
+    float bfSignal[bfsigsize];  //beamformed signal (sum of shifted waveforms)
+
+    //add shifted waveforms together with bfweights into bfSignal
+    for (int i = 0; i < bfsigsize; i++){
+      bfSignal[i] = 0;
+      for (int m = 0; m < 2; m++){
+        bfSignal[i] += samples[m][i+intdly[m]] / bfweights[m];
+      }
+    }
+    scanArea[p] = calcPower(bfSignal, bfsigsize) * avg(bfweights[0], bfweights[1]);
   }  
 }
 
 //calculates power of waveform
-float calcPower(long *waveform, int wsize){
+float calcPower(float *waveform, int wsize){
   float result = 0;
   for (int i = 0; i < wsize; i++){
     result += sq(waveform[i]);
@@ -85,13 +102,13 @@ float calcPower(long *waveform, int wsize){
 }
 
 //removes dc component of waveform
-void centerWaveform(int *waveform, int wsize){
-  long average = 0;
+void centerWaveform(float *waveform, int wsize){
+  float average = 0;
   //calculate mean
   for (int i = 0; i < wsize; i++){
     average += waveform[i];
   }
-  average /= numSamples;
+  average /= wsize;
 
   //subtract mean from each value in waveform[]
   for (int i = 0; i < wsize; i++){
@@ -110,34 +127,33 @@ void printDelays(){
   }
 }
 
-//fill the Delays[][] matrix
+//fill the Delays[][] matrix using calculated sample delay values
+//and also normalise the matrix by converting to relative values
 void calcDelays() {
   for (int i = 0; i < numPoints; i++) {
-    Delays[0][i]
-      = round(sqrt(sq(pointStep * (numPoints / 2 - i) - micDist / 2) + sq(micDist)) * (float) fs / c);
-    Delays[1][i]
-      = round(sqrt(sq(pointStep * (numPoints / 2 - i) + micDist / 2) + sq(micDist)) * (float) fs / c);
+    //calculate sample delay using geometry
+    float d1
+      = sqrt(sq(pointStep * (numPoints / 2 - i) - micDist / 2) + sq(micDist)) * (float) fs / c;
+    float d2
+      = sqrt(sq(pointStep * (numPoints / 2 - i) + micDist / 2) + sq(micDist)) * (float) fs / c;
+    //normalise the delay and round into integers
+    float min_d = min(d1, d2);
+    Delays[0][i] = (d1 - min_d);
+    Delays[1][i] = (d2 - min_d);
   }
 }
 
-/*  calcDelays will calculate the time taken for sound to travel 
- *  from each point to each mic. We need to convert those values 
- *  to relative values, this is done by normaliseDelays()
- */
-void normaliseDelays(){
-  for (int p = 0; p < numPoints; p++){
-    int minDelayPointp = min(Delays[0][p], Delays[1][p]);
-    for (int m = 0; m < 2; m++){
-      Delays[m][p] -= minDelayPointp;
-    }
-  }
+float avg(float a, float b){
+  return (a+b)/2;
 }
 
 //record samples into samples[2][numSamples]
 //should I do a noInterrupt in this callback?
+//callback takes 4-5uS
 void samplingCallback() {
-  samples[0][sampleCounter] = analogRead(micPin1);
-  samples[1][sampleCounter] = analogRead(micPin2);
+  for (int m = 0; m < numMics; m++){
+    samples[m][sampleCounter] = analogRead(micPins[m]);
+  }
   
   sampleCounter++;
   if (sampleCounter >= numSamples) {
