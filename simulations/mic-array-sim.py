43572135cef2c9cc5f9befd2848c116426574ebe
DangerousTim <tpb2k99@gmail.com>
2020-02-12 17:55:32 +0530

    class Mic added

--- simulations/mic-array-sim.py
+++ simulations/mic-array-sim.py
@@ -1,8 +1,16 @@
 import numpy as np
 
+# omnidirectional mic class, limited to 2 dimension for now 
 class Mic:
 	numSamples = 100
-	
+
+	# position must be a tuple of length 2
 	def __init__(self, position):
 		self.position = position
 		self.waveform = np.zeros(self.numSamples, dtype='float32')
+
+# simple sine wave omnidirectional source
+class Source:
+	def __init__(self, position, freq):
+		self.position = position
+		self.freq = freq
