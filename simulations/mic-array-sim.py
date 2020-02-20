937402fdefa37aaa0d8f348ba36254fb1d4ba7a5
DangerousTim <tpb2k99@gmail.com>
2020-02-20 17:10:59 +0530

    simulator generateWaveform added

--- simulations/mic-array-sim.py
+++ simulations/mic-array-sim.py
@@ -1,44 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-# omnidirectional mic class, limited to 2 dimension for now 
-class Mic:
-	numSamples = 100
-	# position must be a tuple of length 2
-	def __init__(self, position):
-		self.position = position
-		self.waveform = np.zeros(self.numSamples, dtype='float32')
-	#func to generate wavefrom for different mics
-	def generateWaveform1(self):
-		global wavef1,wavef2,t
-		t=np.linspace(-np.pi,np.pi,256)
-		wavef1=np.sin(t)
-		wavef2=np.cos(t)
-		Mic.plotwaveform()
-	def plotwaveform():		#to plot the data from different mics
-		fig,ax=plt.subplots(2)
-		ax[0].plot(t,wavef1)
-		ax[0].set_xlabel("Data from mic1")
-		ax[1].plot(t,wavef2)
-		ax[1].set_xlabel("Data from mic2")
-		plt.show()
+
+c = 340		# speed of sound in m/s
+
 # simple sine wave omnidirectional source
 class Source:
 	def __init__(self, position, freq):
 		self.position = position
 		self.freq = freq
 
-# initialise two mic objects in a linear array
-mic1 = Mic((2.0,0.0))
-mic2 = Mic((-2.0,0.0))
-src = Source((3.0,2.0),100.0)
-# init a source somewhere in front of the mics
-fig,axs=plt.subplots(2)
-# generate the mic's output waveform (Mic.generateWaveform())
+# omnidirectional mic class, limited to 2 dimension for now 
+class Mic:
+	numSamples = 100		
+	samplingRate = 1000	# sampling freq in Hz
 
-mic1.generateWaveform1()
-mic2.generateWaveform2()
+	# position must be a tuple of length 2
+	def __init__(self, position):
+		self.position = position
+		self.waveform = np.zeros(self.numSamples, dtype='float32')
+		# t_range useful for generating waveform and plotting
+		self.t_range = np.linspace(0, self.numSamples / self.samplingRate, self.numSamples)
+
+
+	#func to generate wavefrom for different mics
+	def generateWaveform(self, src):
+		# calculate distance between mic and source
+		dist = np.array(src.position) - np.array(self.position)
+		dist = np.sqrt(dist.dot(dist))
+		# calc phase difference
+		phasediff = 2 * np.pi * src.freq * dist / c
+
+		# waveform will be sin(2(pi)vt + phi) 
+		# where v is src.freq
+		self.waveform = np.sin(2*np.pi*src.freq*self.t_range + phasediff)
+
+	# def plotwaveform():		#to plot the data from different mics
+	# 	fig,ax=plt.subplots(2)
+		# ax[0].plot(t,wavef1)
+		# ax[0].set_xlabel("Data from mic1")
+		# ax[1].plot(t,wavef2)
+		# ax[1].set_xlabel("Data from mic2")
+		# plt.show()
+
+# initialise two mic objects in a linear arrangement on x axis
+mic1 = Mic((2.0, 0.0))
+mic2 = Mic((-2.0, 0.0))
+
+# init a source somewhere in front of the mics
+src = Source((0, 2.0), 100.0)
+
+# generate the mic's output waveform 
+mic1.generateWaveform(src)
+mic2.generateWaveform(src)
+
 # plot them
-    
+fig, axs = plt.subplots(2)
+axs[0].plot(mic1.t_range, mic1.waveform)
+axs[0].set_ylabel("Data from mic1")
+axs[1].plot(mic2.t_range, mic2.waveform)
+axs[1].set_ylabel("Data from mic2")
+plt.show()
+
 # add all mics' waveforms together and get resultant power
 
 # repeat these steps for different source positions
